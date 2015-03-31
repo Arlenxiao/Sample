fa1e4f585058096e0fb3c7a7ac217b5dbde9c544
yuqicook@163.com <yuqicook@163.com>
2015-03-31 16:10:42 +0800

    随机方法,测试随机颜色

--- CA001/Program.cs
+++ CA001/Program.cs
@@ -11,6 +11,33 @@ namespace CA001
         static void Main(string[] args)
         {
             //这只是git的一个测试例子而已
+            Test_RNG();
+        }
+
+        /// <summary>随机数测试</summary>
+        static void Test_RNG()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("请按任意键继续,按q退出");
+                var input = Console.ReadKey().KeyChar;
+                if (input == 'q') return;
+                for (int i = 0; i < 100; i++)
+                { 
+                    ConsoleColor[] colors = Enum.GetValues(typeof(ConsoleColor)) as ConsoleColor[]; 
+
+                    var rng=RNG.Next(colors.Length-1);//调用随机方法
+
+                    var c = colors[rng];
+
+                    Console.ForegroundColor = c == ConsoleColor.Black ? ConsoleColor.Red : c;
+
+                    Console.WriteLine(rng);
+                }
+
+            }
+
         }
     }
 }
