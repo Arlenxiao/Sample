a5cb4ff2dfabf1203161ecaac9935f7455c59e98
yuqicook@163.com <yuqicook@163.com>
2015-04-02 17:08:53 +0800

    示例提交

--- CA001/Program.cs
+++ CA001/Program.cs
@@ -12,8 +12,8 @@ namespace CA001
         static void Main(string[] args)
         {
             //这只是git的一个测试例子而已
-            //Test_RNG();
-            Test_格式化磁盘();
+            Test_RNG();
+            //Test_格式化磁盘();
         }
 
 
@@ -37,25 +37,25 @@ namespace CA001
         static void Test_RNG()
         {
 
-            //while (true)
-            //{
-            //    Console.WriteLine("请按任意键继续,按q退出");
-            //    var input = Console.ReadKey().KeyChar;
-            //    if (input == 'q') return;
-            //    for (int i = 0; i < 100; i++)
-            //    { 
-            //        ConsoleColor[] colors = Enum.GetValues(typeof(ConsoleColor)) as ConsoleColor[]; 
+            while (true)
+            {
+                Console.WriteLine("请按任意键继续,按q退出");
+                var input = Console.ReadKey().KeyChar;
+                if (input == 'q') return;
+                for (int i = 0; i < 100; i++)
+                {
+                    var colors = Enum.GetValues(typeof(ConsoleColor)) as ConsoleColor[];
 
-            //        var rng=RNG.Next(colors.Length-1);//调用随机方法
+                    var rng = RNG.Next(colors.Length - 1);//调用随机方法
 
-            //        var c = colors[rng];
+                    var c = colors[rng];
 
-            //        Console.ForegroundColor = c == ConsoleColor.Black ? ConsoleColor.Red : c;
+                    Console.ForegroundColor = c == ConsoleColor.Black ? ConsoleColor.Red : c;
 
-            //        Console.WriteLine(rng);
-            //    }
+                    Console.WriteLine(rng);
+                }
 
-            //}
+            }
 
         }
     }
